7c55e9b45b005aacf5f38c2814f8d43d08bd7fb3
szewczykmira <szewczyk.mira@gmail.com>
2015-02-24 16:42:04 +0100

    Third list fixed

--- Listy/Lista3/rozwiazania.pl
+++ Listy/Lista3/rozwiazania.pl
@@ -14,8 +14,13 @@ exp(_,0,1):-!.
 exp(A,B,C):-exp(A,B1,C1),B is B1+1,C is C1*A.
 exp2(A,B,C):-C is A^B.
 %Zadanie 3
-factorial(0,1):-!.
-factorial(X,Y):-factorial(X1,Y1),X is X1+1,Y is Y1*X.
+factorial(0,1):-
+  !.
+factorial(X,Y):-
+  X > 0,
+  DecreasedX is X-1,
+  factorial(DecreasedX, DecreasedY),
+  Y is DecreasedY * X.
 %
 concat_number(Digits, Num):-
   concat_number(Digits, 0, Num).
@@ -33,19 +38,26 @@ decimal(Num, Acc, Res):-
   NewNum is Num // 10,
   decimal(NewNum, [CurrentDigit|Acc], Res).
 %Zadanie 4
-selectmin([X],X,[]).
-selectmin([H|T],H,[M|R]):-H<M,selectmin(T,M,R).
-selectmin([H|T],M,[H|R]):-H>M,selectmin(T,M,R).
-sel_sort([],[]).
+select_min([Head|Tail], Min, Rest):-
+  select_min(Tail, Min, ReversedRest, [], Head),
+  reverse(ReversedRest, Rest).
+select_min([], Min, Rest, Rest, Min).
+select_min([H|T], Min, Rest, AccRest, CurrentMin):-
+    H >= CurrentMin, !,
+    select_min(T, Min, Rest, [H|AccRest], CurrentMin).
+select_min([H|T], Min, Rest, AccRest, CurrentMin):-
+    H < CurrentMin,
+    select_min(T, Min, Rest, [CurrentMin|AccRest], H).
+sel_sort([],[]):-!.
 sel_sort(X,[H|T]):-selectmin(X,H,R),sel_sort(R,T).
 %Zadanie 5
 insert([],Elem,[Elem]).
 insert([H|T],Elem, [Elem,H|T]):-Elem=<H,!.
-insert([H|T],Elem,[H|R]):-Elem>H,insert(T,Elem,[H|R]).
+insert([H|T],Elem,[H|R]):-Elem>H,insert(T,Elem,R).
 
 ins_sort([],[]).
 ins_sort([H|T],SL):-ins_sort(T,SL1),insert(SL1,H,SL).
 %Zadanie 6
 reverse(X,Y):-reverse(X,[],Y).
 reverse([],A,A).
-reverse([H|T],A,Y):-reverse(T,[H|A],Y).
+reverse([H|T],A,Y):-!,reverse(T,[H|A],Y).
