ae1aedb363f683b1d6005f8f3f11e9f1a90ea481
szewczykmira <szewczyk.mira@gmail.com>
2015-03-17 22:30:10 +0100

    Second

--- Listy/Lista5/rozwiazania.pl
+++ Listy/Lista5/rozwiazania.pl
@@ -3,3 +3,9 @@ flatten(List, Flat):- flatten(List, Flat, []).
 flatten([], Flat, Flat).
 flatten([H|T],[H|F],Acc):- atomic(H),!,flatten(T,F,Acc).
 flatten([H|T],Flat,Acc):-flatten(T, TailF, Acc), flatten(H, Flat, TailF).
+
+%Zadanie 2
+halve(List, Left, Right):- halve(List, List, Left, Right).
+halve(Right, [],[],Right):-!.
+halve(Right, [_],[],Right):-!.
+halve([H|T],[_,_|Rest],[H|Left],Right):-halve(T,Rest, Left,Right).
