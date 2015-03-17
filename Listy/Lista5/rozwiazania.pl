99b541c7775415b0cb3ca6fe53a83dfaac316b37
szewczykmira <szewczyk.mira@gmail.com>
2015-03-17 22:35:14 +0100

    Third

--- Listy/Lista5/rozwiazania.pl
+++ Listy/Lista5/rozwiazania.pl
@@ -9,3 +9,13 @@ halve(List, Left, Right):- halve(List, List, Left, Right).
 halve(Right, [],[],Right):-!.
 halve(Right, [_],[],Right):-!.
 halve([H|T],[_,_|Rest],[H|Left],Right):-halve(T,Rest, Left,Right).
+
+%Zadanie 3
+merge([],List, List):-!.
+merge(List,[],List):-!.
+merge([H1|T1],[H2|T2],[H1|Rest]):- H1=<H2, merge(T1,[H2|T2],Rest).
+merge(List, [H|T],[H|Rest]):- merge(List, T,Rest).
+
+merge_sort([],[]).
+merge_sort([X],[X]).
+merge_sort(List,Result):-halve(List, Left, Right), merge_sort(Left,ResLeft),merge_sort(Right, ResRight), merge(ResLeft,ResRight,Result).
