1ac09ab7a8ea56dfc994eb0136eb0d1ef7246e70
szewczykmira <szewczyk.mira@gmail.com>
2015-03-17 22:40:39 +0100

    Fifth

--- Listy/Lista5/rozwiazania.pl
+++ Listy/Lista5/rozwiazania.pl
@@ -19,3 +19,12 @@ merge(List, [H|T],[H|Rest]):- merge(List, T,Rest).
 merge_sort([],[]).
 merge_sort([X],[X]).
 merge_sort(List,Result):-halve(List, Left, Right), merge_sort(Left,ResLeft),merge_sort(Right, ResRight), merge(ResLeft,ResRight,Result).
+
+%Zadanie 5
+split([],_,[],[]):-!.
+split([E|List],Med,Small,[E|Big]):- E>=Med,!, split(List, Med, Small, Big).
+split([E|List],Med, [E|Small], Big):- split(List, Med, Small, Big).
+
+qsort(List, Sorted):- qsort(List, Sorted, []).
+qsort([], Sorted,Sorted).
+qsort([E|Tail], Sorted, Acc):- split(Tail, E,Small, Big), qsort(Big, NextAcc, Acc), qsort(Small, Sorted, [E|NextAcc]).
