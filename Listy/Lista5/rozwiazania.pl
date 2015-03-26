baf0d71b3f9c4328fbdd87642cb4d93b77a6a3ab
szewczykmira <szewczyk.mira@gmail.com>
2015-03-26 19:54:55 +0100

    Tasks numbers fixed

--- Listy/Lista5/rozwiazania.pl
+++ Listy/Lista5/rozwiazania.pl
@@ -8,13 +8,13 @@ flatten([], Flat, Flat).
 flatten([H|T],[H|F],Acc):- atomic(H),!,flatten(T,F,Acc).
 flatten([H|T],Flat,Acc):-flatten(T, TailF, Acc), flatten(H, Flat, TailF).
 
-%Zadanie 2
+%Zadanie 3
 halve(List, Left, Right):- halve(List, List, Left, Right).
 halve(Right, [],[],Right):-!.
 halve(Right, [_],[],Right):-!.
 halve([H|T],[_,_|Rest],[H|Left],Right):-halve(T,Rest, Left,Right).
 
-%Zadanie 3
+%Zadanie 4
 merge([],List, List):-!.
 merge(List,[],List):-!.
 merge([H1|T1],[H2|T2],[H1|Rest]):- H1=<H2, merge(T1,[H2|T2],Rest).
@@ -24,7 +24,7 @@ merge_sort([],[]).
 merge_sort([X],[X]).
 merge_sort(List,Result):-halve(List, Left, Right), merge_sort(Left,ResLeft),merge_sort(Right, ResRight), merge(ResLeft,ResRight,Result).
 
-%Zadanie 5
+%Zadanie 7
 split([],_,[],[]):-!.
 split([E|List],Med,Small,[E|Big]):- E>=Med,!, split(List, Med, Small, Big).
 split([E|List],Med, [E|Small], Big):- split(List, Med, Small, Big).
