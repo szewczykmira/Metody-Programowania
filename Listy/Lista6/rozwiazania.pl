50a41095eb18b4cabe956ecbfcea29302242b9f7
szewczykmira <szewczyk.mira@gmail.com>
2015-04-11 15:59:08 +0200

    Insert

--- Listy/Lista6/rozwiazania.pl
+++ Listy/Lista6/rozwiazania.pl
@@ -9,3 +9,11 @@ putr(E, Li-[E|L], Li-L).
 getr([E|L]-X,E, L-X).
 emptyr(Y-Y).
 %addallr
+
+%Zadanie3
+
+insert(Elem, leaf, node(leaf, Elem, leaf).
+insert(Elem, node(Left,Root,Right),node(Left,Root,InsRight):- Elem > Root, insert(Elem,Right,InsRight).
+insert(Elem, node(Left,Root,Right), node(InsLeft,Root,Right):- Elem < Root, insert(Elem, Left, InsLeft).
+insert(Elem, node(Left,E,Rigth), node(Left,E,Right).
+
