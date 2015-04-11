5ef58fb37fa79f7b6d5f58c48514fc817ec61c4f
szewczykmira <szewczyk.mira@gmail.com>
2015-04-11 16:10:46 +0200

    3rd finished

--- Listy/Lista6/rozwiazania.pl
+++ Listy/Lista6/rozwiazania.pl
@@ -17,3 +17,19 @@ insert(Elem, node(Left,Root,Right),node(Left,Root,InsRight):- Elem > Root, inser
 insert(Elem, node(Left,Root,Right), node(InsLeft,Root,Right):- Elem < Root, insert(Elem, Left, InsLeft).
 insert(Elem, node(Left,E,Rigth), node(Left,E,Right).
 
+find(Elem, node(_,Root,_)):-Elem==Root,!.
+find(Elem, node(Left,Root,Right)):- Elem<Root, find(Elem,Left).
+find(Elem, node(Left,Root,Right)):- Elem>Root, find(Elem,Right).
+
+findMax(node(_,Root,leaf),Root):-!.
+findMax(node(_,_,Right),Max):- findMax(Right,Max).
+
+delete(_,leaf,leaf):-!.
+delete(Elem,node(leaf,Elem,leaf),leaf):-!.
+delete(Elem,node(Left,Elem,leaf),Left):-!.
+delete(Elem,node(leaf,Elem,Right),Right):-!.
+delete(Elem,node(Left,Elem,Right),node(LeftWM,LeftMax,Right):- findMax(Left,LeftMax), delete(LeftMax,Left,LeftWM).
+
+delMax(Begin,End,Elem):- findMax(Begin,Elem), delete(Elem,Begin,End).
+
+empty(leaf).
