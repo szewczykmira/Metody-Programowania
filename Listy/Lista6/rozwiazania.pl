de21783ac71558db5bcd2b95b1a9da3038e9e81e
szewczykmira <szewczyk.mira@gmail.com>
2015-04-15 19:40:37 +0200

    End of 6th list

--- Listy/Lista6/rozwiazania.pl
+++ Listy/Lista6/rozwiazania.pl
@@ -28,8 +28,146 @@ delete(_,leaf,leaf):-!.
 delete(Elem,node(leaf,Elem,leaf),leaf):-!.
 delete(Elem,node(Left,Elem,leaf),Left):-!.
 delete(Elem,node(leaf,Elem,Right),Right):-!.
-delete(Elem,node(Left,Elem,Right),node(LeftWM,LeftMax,Right):- findMax(Left,LeftMax), delete(LeftMax,Left,LeftWM).
+delete(Elem,node(Left,Elem,Right),node(LeftWM,LeftMax,Right)):- findMax(Left,LeftMax), delete(LeftMax,Left,LeftWM).
 
 delMax(Begin,End,Elem):- findMax(Begin,Elem), delete(Elem,Begin,End).
 
 empty(leaf).
+%Zadanie5
+TBA
+
+%Zadanie6
+% 1
+% {0^n 1^m 0^n | n,m<N}
+% <G={0,1},{S,M}, S, S-> 0S0 | M | E, M -> 1M | E>
+
+% 2
+% {0^n 1^n 0^m | n,m <N}
+% <G={0,1}, {S,M}, S, S-> S0 | T0 | E, T-> 0T1 | E >
+
+% 3
+% {0^n 1^m 0^k | n,m,k<N }
+3. G = <E,V,S,P>
+
+E = {0,1}
+V = {S,T}
+S = S
+P = {
+  S -> S0
+  S -> T
+  S -> e
+
+  T -> T1
+  T -> 0T1
+  T -> e
+    }
+
+4. G = <E,V,S,P>
+
+  E = {0,1}
+  V = {S}
+  S = S
+  P = {
+  S -> 01S00
+  S -> e
+  }
+
+5. G = <E,V,S,P>
+
+  E = {0,1}
+  V = {S}
+  S = S
+  P = {
+    S -> 0S1
+    S -> 1S0
+    S -> SS // Zauważmy, że to przyda się przy generowaniu 1010
+    S -> e
+  }
+
+6. G = <E,V,S,P>
+
+  E = {0,1}
+  V = {S}
+  S = S
+  P = {
+    S -> 1S0S0
+    S -> 0S1S0
+    S -> 0S0S1
+    S -> e
+  }
+
+ %Zadanie7 
+
+1)  G = <E,V,S,P>
+    E = {0}
+    V = {S}
+    S = S
+    P = {
+    S -> 00S
+    S -> e
+ }
+
+2)  G = <E,V,S,P>
+    E = {0,1}
+    V = {S,T}
+    S = S,
+    P = {
+    S -> 0S
+    S -> M
+    S -> e
+    M -> 1M
+    M -> e
+ }
+
+3)  G = <E,V,S,P>
+    E = {0,1}
+    V = {S,T}
+    S = S
+    P = {
+    S -> 0S
+    S -> 1M
+    S -> 11M
+    S -> e
+    M -> 0S
+    M -> e
+  }
+
+4)  G = <E,V,S,P>
+    E = {0,1}
+    V = {S,T}
+    S = S
+    P = {
+    S -> 0T
+    S -> 1S
+    T -> 0S
+    T -> 1T
+    }
+
+5)  G = <E,V,S,P>
+    E = {0,1}
+    V = {S,T,U,W}
+    S = S
+    P = {
+    S -> 0T
+    S -> 1W // być może koniec
+    T -> 0S
+    T -> 1U
+
+    U -> 0W // być może koniec
+    U -> 1T
+    W -> 0U
+    W -> 1S
+    W -> e
+    }
+
+6)  G = <E,V,S,P>
+    E = {0,1}
+    V = {S,T}
+    S = S
+    P = {
+    S -> 0T
+    S -> 1T
+    S -> e
+    T -> 0S
+    T -> 1T
+    }
