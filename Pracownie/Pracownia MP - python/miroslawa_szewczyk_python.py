1f21ce0426d725f0e15dd0e5a54b754451789056
Mira <szewczyk.mira@gmail.com>
2016-03-15 16:10:02 +0100

    Finished version

--- Pracownie/Pracownia MP - python/miroslawa_szewczyk_python.py
+++ Pracownie/Pracownia MP - python/miroslawa_szewczyk_python.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import math
+import sys
 
 PREC = 4
 
@@ -81,11 +82,26 @@ class Direction(Vector):
     def further(d):
         return d.roll(math.pi/2)
 
+    def reverse(rot, d):
+        return {
+                "left" : Direction.right,
+                "right" : Direction.left,
+                "counterclockwise" : Direction.clockwise,
+                "clockwise" : Direction.counterclockwise,
+                "closer" : Direction.further,
+                "further" : Direction.closer
+                }[rot.__name__](d)
+
 class Pen:
-    def __init__(self, unit=250):
-        self.unit = unit
-        # self.scalable_unit = 1. / (pow(2,self.n) - 1)
+    def __init__(self, n, pic_size, edge, observer, x, y, z, angle_x, angle_y):
         self.path = [Vector(0,0,0)]
+        self.edge = edge #edge / (math.pow(2, n) - 1)
+        self.x = x
+        self.y = y
+        self.z = z
+        self.pic_size = pic_size
+        self.angle_x = math.radians(angle_x)
+        self.angle_y = math.radians(angle_y)
 
     def last(self):
         return self.path[-1]
@@ -96,8 +112,7 @@ class Pen:
     def header(self, max_x, max_y):
         return  """%IPS-Adobe-2.0 EPSF-2.0
 %%BoundingBox: -1 -1 {0} {1}
-newpath
-0.0 0.0 moveto""".format(max_x, max_y)
+newpath""".format(max_x, max_y)
 
     def footer(self):
         return """.4 setlinewidth
@@ -106,163 +121,179 @@ showpage
 %%Trailer
 %EOF"""
 
-    def print_path_to_postscript(self):
-        xs = [x.x for x in self.path]
-        ys = [x.y for x in self.path]
-        max_xs = max(xs)
-        max_ys = max(ys)
-        print(self.header(max_xs + 1, max_ys + 1))
-        for p in self.path:
-            x, y = p
+    def print_path(self):
+        print(self.header(self.pic_size, self.pic_size))
+        x, y, _ = self.project(self.path[0]).vector()
+        print(float(x), float(y), "moveto")
+        for current in self.path:
+            x,y,_ = self.project(current).vector()
             print(float(x), float(y), "lineto")
         print(self.footer())
 
-    def print_path(self):
-        xs = [x.x for x in self.path]
-        ys = [x.y for x in self.path]
-        print(self.header(max_xs + 1, max_ys +1))
-        last = [0,0,0]
-        last_real = Vector(0,0,0).vector
-        for current in self.paht:
-            vec = current.vector
-            vector = [vec[0] - last_real[0],
-                    vec[1] - last_real[1], vec[2] - last_real[2]]
-            last = self.postscript_move(last, vector)
-            last_real = vec
-        print(self.footer())
+    def rotate(self, vec):
+        rot_x = Direction.roll(vec, self.angle_x)
+        rot_y = Direction.pitch(rot_x, self.angle_y)
+        return rot_y
 
-    def postscript_move(self, last, vector):
-        x, y, z = last
-        x1, y1, z1 = vector
-        if x is not 0:
-            print(float(x) + float(x1), float(y), float(z), "lineto")
-            return [x+x1, y, z]
-        elif y is not 0:
-            print(float(x), float(y) + float(y1), float(z), "lineto")
-            return [x, y + y1, z]
-        else:
+    def translate(self, vec):
+        x,y,z = vec.vector()
+        return Vector(x + self.x, y + self.y, z + self.z)
 
+    def lengthen(self, vec):
+        x,y,z = vec.vector()
+        return Vector(x * self.edge, y * self.edge, z * self.edge)
+
+    def project(self, vec):
+        return self.rotate(self.translate(self.lengthen(vec)))
 
 
 class Tortoise:
-    def __init__(self):
-        self.pen = Pen()
+    def __init__(self, pen):
+        self.pen = pen
         self.rotations = []
+
+    def local_to_global(self, v):
+        for r in self.rotations[::-1]:
+            v = Direction.reverse(r, v)
+        return v
 
     def current_direction(self):
         d = Direction.inside()
-        for r in self.rotations:
-            d = r(d)
-        return d
+        return self.local_to_global(d)
 
-    def forward(self):
-        d = self.current_direction()
-        print("->", d)
+    def current_top(self):
+        d = Direction.upside()
+        return self.local_to_global(d)
+
+    def coords(self):
+        return self.current_direction(), self.current_top()
+
+    def forward(self, direction = Direction.inside()):
+        d = self.local_to_global(direction)
         self.pen.move(d)
 
     def rotate(self, rotation):
-        print(rotation.__name__)
         self.rotations.append(rotation)
+
+    def back(self, n = 1):
+        self.rotations = self.rotations[:len(self.rotations) - n]
 
 
 class HillbertFractal:
-    def __init__(self):
-        self.tortoise = Tortoise()
+    def __init__(self, n=1, size=500, length=None, observer=10,
+            x=0, y=0, z=0, angle1=90, angle2=90):
+        if not length:
+            length = math.pow(2,n)
+        pen = Pen(n, size, float(length), observer, float(x), float(y), float(z), float(angle1), float(angle2))
+        self.tortoise = Tortoise(pen)
 
+    
     # Tortoise should end up facing same direction he started
-    def draw(self, n):
+    def draw(self, n, rotate = False):
         if n == 0:
             return
 
+        right = Direction.right if rotate else Direction.left
+        left = Direction.left if rotate else Direction.right
+        closer = Direction.closer if rotate else Direction.further
+        further = Direction.further if rotate else Direction.closer
+        clockwise = Direction.clockwise if rotate else Direction.counterclockwise
+        counterclockwise = Direction.counterclockwise if rotate else Direction.clockwise
+
+        rightside = Direction.rightside() if rotate else Direction.leftside()
+        leftside = Direction.leftside() if rotate else Direction.rightside()
+        downside = Direction.downside() if rotate else Direction.upside()
+        upside = Direction.upside() if rotate else Direction.downside()
+        inside = Direction.inside() if rotate else Direction.outside()
+        outside = Direction.outside() if rotate else Direction.inside()
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.right)
-        self.tortoise.rotate(Direction.counterclockwise)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(counterclockwise)
         # I.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.clockwise)
-        self.tortoise.rotate(Direction.left)
-        # Rotate to face up
-        self.tortoise.rotate(Direction.closer)
-        self.tortoise.forward()
+        self.tortoise.back(2)
+
+        self.tortoise.forward(upside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.clockwise)
+        self.tortoise.rotate(left)
+        self.tortoise.rotate(closer)
         # II.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.counterclockwise)
-        # Rotate to face inside
-        self.tortoise.rotate(Direction.further)
-        self.tortoise.forward()
+        self.tortoise.back(2)
+
+        self.tortoise.forward(inside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.closer)
-        self.tortoise.rotate(Direction.counterclockwise)
+        self.tortoise.rotate(left)
+        self.tortoise.rotate(closer)
         # III.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.clockwise)
-        self.tortoise.rotate(Direction.further)
-        # Rotate to face down
-        self.tortoise.rotate(Direction.further)
-        self.tortoise.forward()
+        self.tortoise.back(2)
+
+        self.tortoise.forward(downside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.further)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(clockwise)
+        self.tortoise.rotate(clockwise)
         # IV.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.closer)
-        # Rotate to face right
-        self.tortoise.rotate(Direction.right)
-        self.tortoise.forward()
+        self.tortoise.back(4)
+
+        self.tortoise.forward(rightside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.further)
-        self.tortoise.rotate(Direction.counterclockwise)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(clockwise)
+        self.tortoise.rotate(clockwise)
         # V.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.clockwise)
-        self.tortoise.rotate(Direction.closer)
-        # Rotate to face up
-        self.tortoise.rotate(Direction.right)
-        self.tortoise.forward()
+        self.tortoise.back(4)
+
+        self.tortoise.forward(upside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.clockwise)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(closer)
         # VI.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.counterclockwise)
-        # Rotate to face outside
-        self.tortoise.rotate(Direction.further)
-        self.tortoise.forward()
+        self.tortoise.back(2)
+
+        self.tortoise.forward(outside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.closer)
-        self.tortoise.rotate(Direction.clockwise)
+        self.tortoise.rotate(right)
+        self.tortoise.rotate(closer)
         # VII.
-        self.draw(n-1)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.counterclockwise)
-        self.tortoise.rotate(Direction.further)
-        # Rotate to face downward
-        self.tortoise.rotate(Direction.further)
-        self.tortoise.forward()
+        self.tortoise.back(2)
+
+        self.tortoise.forward(downside)
+
         # Prepare to draw n-1
-        self.tortoise.rotate(Direction.right)
-        self.tortoise.rotate(Direction.clockwise)
-        self.tortoise.rotate(Direction.clockwise)
         # VIII.
-        self.draw(n-1)
+        self.tortoise.rotate(left)
+        self.tortoise.rotate(clockwise)
+        self.draw(n-1, rotate)
         # Rotate back after
-        self.tortoise.rotate(Direction.counterclockwise)
-        self.tortoise.rotate(Direction.counterclockwise)
-        self.tortoise.rotate(Direction.left)
-        # Rotate inside
-        self.tortoise.rotate(Direction.closer)
-        # Rotate face 2x
-        self.tortoise.rotate(Direction.counterclockwise)
-        self.tortoise.rotate(Direction.counterclockwise)
+        self.tortoise.back(2)
+
         return self
 
     def path(self):
         return self.tortoise.pen.path
 
-print(HillbertFractal().draw(1).path())
+
+if __name__ == "__main__":
+    HillbertFractal(*sys.argv[2:]).draw(int(sys.argv[2])).tortoise.pen.print_path()
