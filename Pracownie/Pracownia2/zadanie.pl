b53874b0ef50ea14d9f24c4b64ff77d1bb13b634
Mira <szewczyk.mira@gmail.com>
2016-03-25 21:35:41 +0100

    For and numerize added

--- Pracownie/Pracownia2/zadanie.pl
+++ Pracownie/Pracownia2/zadanie.pl
@@ -13,3 +13,9 @@ neg(A, ~A).
 
 convert([],[]).
 convert([H|T], [(H, axiom) | Z]) :- convert(T, Z).
+
+numerize([],_, []).
+numerize([H|T], A, [B|C]) :- B = (H, A),A1 is A +1, numerize(T, A1, C).
+
+for(_, [], []):-!.
+for(A, [H|T], [X|Y]) :- X = [A, H], for(A, T, Y).
