d68d1176e45496ec1b3e4977e9c599d83917a834
Mira <szewczyk.mira@gmail.com>
2016-03-29 18:39:08 +0200

    Some cleanup

--- Pracownie/Pracownia2/zadanie.pl
+++ Pracownie/Pracownia2/zadanie.pl
@@ -14,8 +14,16 @@ neg(A, ~A).
 convert([],[]).
 convert([H|T], [(H, axiom) | Z]) :- convert(T, Z).
 
+
+
+% Temporary unneeded
 numerize([],_, []).
 numerize([H|T], A, [B|C]) :- B = (H, A),A1 is A +1, numerize(T, A1, C).
 
 for(_, [], []):-!.
-for(A, [H|T], [X|Y]) :- X = [A, H], for(A, T, Y).
+for(A, [H|T], [X|Y]) :- X = (A, H), for(A, T, Y).
+
+forfor(A,B) :- for2(A, [], [], B).
+
+for2([], _, B, B).
+for2([H|T],Z,A, _) :- for(H, Z, B), append(Z,[H],Y), append(A, B, C), for2(T, Y, C, _).
