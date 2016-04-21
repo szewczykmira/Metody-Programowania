2b157fefa1d58e95535fba626c3e850a72065b60
Mira <szewczyk.mira@gmail.com>
2016-04-21 22:04:45 +0200

    Numbers and identifiers defined

--- Pracownie/Compiler/compiler.pl
+++ Pracownie/Compiler/compiler.pl
@@ -17,4 +17,16 @@ mul_op --> "mod".
 add_op --> "+".
 add_op --> "-".
 
-% 
+% digits and numbers
+digit(D) --> [D], {code_type(D, digit)}.
+digits([]) --> [].
+digits([H|T]) --> digit(H),!, digits(T).
+number(D, N) --> digits(Ds), { number_chars(N, [D|Ds]) }.
+
+% letters and words
+letter(L) --> [L], {code_type(L, alpha)}.
+
+% identifier
+ident([H|T]) --> [H], {code_type(H, csym); H=39},!, ident(T).
+ident([]) --> [].
+identifier([H|T]) --> letter(H), ident(T).
