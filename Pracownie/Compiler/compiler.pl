d4a074818b146518349010cf73e4fd9854776516
Mira <szewczyk.mira@gmail.com>
2016-04-21 16:50:36 +0200

    Relational operators

---
+++ Pracownie/Compiler/compiler.pl
@@ -0,0 +1,9 @@
+% first of all we need to define parser for algol16
+
+% relational operators
+rel_op --> "<".
+rel_op --> "<=".
+rel_op --> ">".
+rel_op --> ">=".
+rel_op --> "=".
+rel_op --> "<>".
