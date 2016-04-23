beb5f456c872aee889fd464608c5302fe2ec4ae1
Mira <szewczyk.mira@gmail.com>
2016-04-23 15:53:27 +0200

    added formal_arg_str

--- Pracownie/Compiler/compiler.pl
+++ Pracownie/Compiler/compiler.pl
@@ -7,7 +7,6 @@
 % blok --> deklaracje "begin" instrukcja_zlozona "end"
 % procedura --> "procedure" nazwa_procedury "(" argumenty_formalne ")" blok
 % argumenty_formalne --> puste | ciąg_argumentow_formalnych
-% ciąg_argumentów_formalnych --> argument_formalny | ciąg_argumentów_formalnych "," argument_formalny
 
 % instrukcja_zlozona --> instrukcja | instrukcja_zlozona ";" instrukcja
 % instrukcja --> zmiena ":=" wyrazenie_arytmetyczne | "if" wyrazenie_logiczne "then" instrukcja_zlozona "fi" | "if" wyrazenie_logiczne "then" instrukcja_zlozona "else" instrukcja_zlozona "fi" | "while" wyrazenie_logiczne "do" instrukcja_zlozona "done" | "call" wywolanie_procedury | "return" wyrazenie_arytmetyczne | "read" zmienna | "write" wyrazenie_arytmetyczne
@@ -78,8 +77,12 @@ variables --> variable.
 variables --> variables,!, ",", variable.
 
 % formal argument
-formal_arg --> variable.
 formal_arg --> "value", variable.
+formal_arg --> variable.
+
+% string of formal arguments
+formal_arg_str --> formal_arg,",",formal_arg_str.
+formal_arg_str --> formal_arg.
 
 % procedure name
 proc_name --> identifier.
@@ -117,12 +120,18 @@ test_variables(["a112 or a112,aa not parsed by variables"]).
 test_formal_arg([]) :- test_phrase("a112", formal_arg), test_phrase("valuea112", formal_arg).
 test_formal_arg(["a112 or valuea112 not parsed by formal_arg"]).
 
+test_formal_arg_str() :- test_phrase("aqwer", formal_arg_str), test_phrase("awer,wet3", formal_arg_str), test_phrase("awe,valuewe2", formal_arg_str).
+test_formal_arg_str(["aqwer, <<awer,wet3>> and <<awe,valuewe2>> not parsed by formal_arg_str"]).
+
 test_proc_name([]) :- test_phrase("a112", proc_name).
 test_proc_name(["a112 not parsed by proc_name"]).
+
+test_declarator([]) :- test_phrase("locala112", declarator).
+test_declarator(["locala112 not parsed by delarator"]).
 
 test_all([]).
 test_all([H | T]) :- call(H, E), (E = [] ; print(E)), test_all(T).
 
 :- test_all([
-  test_identifier, test_digit, test_digits, test_variable, test_variables, test_formal_arg, test_proc_name
+  test_identifier, test_digit, test_digits, test_variable, test_variables, test_formal_arg, test_proc_name, test_declarator, test_formal_arg_str
 ]).
