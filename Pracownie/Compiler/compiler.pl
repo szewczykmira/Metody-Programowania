a542ecc19c34a9ca1cdc1e9a2a71abeb8118ab18
Mira <szewczyk.mira@gmail.com>
2016-05-15 14:23:10 +0200

    Rewrite evalstack, funcstack and variables!!!!

--- Pracownie/Compiler/compiler.pl
+++ Pracownie/Compiler/compiler.pl
@@ -879,7 +879,8 @@ program(Ast, Compiled) :-
   Full = [const, evalstack, swapa, const, 8193, store,
  const, funcstack, swapa, const, 16384, store, next("__main__") | LwL],
   fix_labels(Full, 0, LwPL, Ls),
-  fix_func_jumps(LwPL, Ls, Compiled),
+  fix_func_jumps(LwPL, Ls, Compiled1),
+  rewrite_variables(Compiled1, Compiled),
   !.
 
 indent(0) :- !.
@@ -1068,7 +1069,6 @@ unique_variables(if(Logic,Then), VariableList, if(NLogic, NThen)) :-
   unique_variables(Logic, VariableList, NLogic),
   unique_variables(Then, VariableList, NThen).
 
-%FIXME
 unique_variables(assign(Var, Val), VariableList, assign(NVar, NVal)) :-
   unique_variables(Val, VariableList, NVal),
   member((Var, NVar), VariableList).
@@ -1085,27 +1085,18 @@ unique_variables(program(Id, Block), VariableList, program(Id, NewBlock)) :-
 
 unique_variables(declarations([]), A, A, F, F, declarations([])) :-!.
 unique_variables(declarations([H|T]), ValueList, NewValueList, Free, Taken1, declarations([NH | NT])):-
-  % FIXME
   unique_variables(H, ValueList, NewVL, Free, Taken, NH),
-  % FIXME
   unique_variables(declarations(T), NewVL, NewValueList, Taken, Taken1, declarations(NT)).
-  % FIXME
-  %append(NewVL, NVL, NewValueList).
 
-% FIXME
 unique_variables(local([]), ValList, ValList, F, F, local([])):-!.
-% FIXME
 unique_variables(local([H|T]), ValueList, NVal,Free, Taken, local([uvar(Free) | NT])):-
   F1 is Free + 1,
-% FIXME
   unique_variables(local(T), [(H, uvar(Free)) | ValueList], NVal, F1, Taken, local(NT)).
 
 unique_variables(procedure(Id, FA, B), ValList, ValList, Free, Taken1, procedure(Id, NFA, NB)):-
   handleFA(FA, ValList, Nv, Free, Taken, NFA),
-  % FIXME
   unique_variables(B, Nv, NB, Taken, Taken1).
 
-% FIXME?
 unique_variables(block(Dec, CI), VariableList, block(NewDec, NewCI), Free, Taken) :-
   unique_variables(Dec, VariableList, NewValList, Free, Taken, NewDec),
   unique_variables(CI, NewValList, NewCI).
@@ -1117,3 +1108,35 @@ handleFA([value(H)|T], NV, NV1, F, Tk, [value(uvar(F)) | NT]) :-
 handleFA([name(H)|T], NV, NV1, F, Tk, [name(uvar(F)) | NT]) :-
   F1 is F+1,
   handleFA(T, [(H, uvar(F)) | NV], NV1, F1, Tk, NT).
+
+rewrite_variables(Code, NewCode) :-
+  length(Code, LC),
+  LCode is LC + 1,
+  rewrite(Code, NewCode, LCode, []).
+
+rewrite([], [], _, _) :- !.
+rewrite([uvar(X) | T], [R | RewriteT], N, List) :-
+  member((uvar(X), R), List),!,
+  rewrite(T, RewriteT, N, List).
+rewrite([uvar(X) | T], [N | RewriteT], N, List) :-
+  N1 is N+1,
+  rewrite(T, RewriteT, N1, [(uvar(X), N) | List]).
+rewrite([evalstack | T], [R | RewriteT], N, List) :-
+  member((evalstack, R), List),!,
+  rewrite(T, RewriteT, N, List).
+rewrite([evalstack | T], [N | RewriteT], N, List) :-
+  N1 is N+1,
+  rewrite(T, RewriteT, N1, [(evalstack, N) | List]).
+rewrite([funcstack | T], [R | RewriteT], N, List) :-
+  member((funcstack, R), List),!,
+  rewrite(T, RewriteT, N, List).
+rewrite([funcstack | T], [N | RewriteT], N, List) :-
+  N1 is N+1,
+  rewrite(T, RewriteT, N1, [(funcstack, N) | List]).
+rewrite([H|T],[H|RT], N, List) :-
+  rewrite(T, RT, N, List).
+
+% main program
+algol16(Source, Sextium) :-
+  phrase(program(AST), Source),
+  program(AST, Sextium).
