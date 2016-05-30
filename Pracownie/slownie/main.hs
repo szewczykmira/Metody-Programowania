43dce54b5c582abfa430ddf54e49ca9a35785fb0
Mira <szewczyk.mira@gmail.com>
2016-05-30 21:35:15 +0200

    Some little changes and few alterations added

---
+++ Pracownie/slownie/main.hs
@@ -0,0 +1,74 @@
+import Slownie
+
+-- data Waluta = Waluta {
+--  mianownik_poj :: String, 
+--  mianownik_mn :: String,
+--  dopelniacz_mn :: String,
+--  rodzaj :: Rodzaj
+--  } deriving Show
+
+-- AUD : dolar australijski
+defi "AUD" = (Waluta "dolar australijski" "dolary australijskie" "dolarow austalijskich" Meski)
+
+-- BGN : lew bułgarski
+defi "BGN" = (Waluta "lew bulgarski" "lwy bulgarskie" "lwow bulgarskich" "lwow bulgarskich" Meski)
+
+-- BRL : real brazylijski
+defi "BRL" = (Waluta "real brazylijski" "reale brazylijskie" "reali brazylijskich" Meski)
+
+-- BYR : białoruski rubel
+defi "BYR" = (Waluta "bialoruski rubel" "bialoruskie ruble" "bialoruskich rubli" Meski)
+
+-- CAD : dolar kanadyjski
+defi "CAD" = (Waluta "dolar kanadyjski" "dolary kanadyjskie" "dolarow kanadyjskich" Meski)
+
+-- CHF : frank szwajcarski
+defi "CHF" = (Waluta "frank szwajcarski" "franki szwajcarskie" "frankow szwajcarskich" Meski)
+
+-- CNY : juan
+defi "CNY" = (Waluta "juan" "juany" "juanow" Meski)
+
+-- CZK : korona czeska
+defi "CZK" = (Waluta "korona czeska" "korony czeskie" "koron czeskich" Zenski)
+
+-- DKK : korona dunska
+defi "DKK" = (Waluta "korona dunska" "korony dunskie" "koron dunskich" Zenski)
+
+-- EUR : euro
+defi "EUR" = (Waluta "euro" "euro" "euro" Nijaki)
+
+-- GBP : funt brytyjski
+defi "GBP" = (Waluta "funt brytyjski" "funty brytyjskie" "funtow brytyjskich" Meski)
+
+-- HKD : dolar
+defi "HKD" = (Waluta "dolar" "dolary" "dolarow" Meski)
+
+-- HRK : kuna chorwacka
+defi "HKR" = (Waluta "kuna chorwacka" "kuny chorwackie" "kun chorwackich" Zenski)
+
+-- HUF : forint węgierki
+defi "HUF" = (Waluta "forint wegierski" "fortinty wegierskie" "forintow wegierskich" Meski)
+
+-- IDR : rupia
+defi "IDR" = (Waluta "rupia" "rupie" "rupii" Zenski)
+
+-- ISK : korona
+defi "ISK" = (Waluta "korona" "korony" "koron" Zenski)
+-- JPY : jen japonski
+-- KRW : won
+-- MXN : peso
+-- MYR : ringgit
+-- NOK : korona norweska
+-- NZD : dolar
+-- PHP : peso
+-- PLN : zloty
+-- RON : lej rumunski
+-- RUB : rubel rosyjski
+-- SDR : sdr
+-- SEK : korona szwedzka
+-- SGD : dolar
+-- THB : bat
+-- TRY : lira turecka
+-- UAH : hrywna ukraińska
+-- USD : dolar amerykański
+-- ZAR : rand
