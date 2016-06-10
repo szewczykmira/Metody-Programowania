931dd9821ac8516dd96c0a6622050cb2608534e0
Mira <szewczyk.mira@gmail.com>
2016-06-10 18:59:48 +0200

    Fix

--- Pracownie/slownie/test.py
+++ Pracownie/slownie/test.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import random
 
 def test_currency(currency, begin, end):
-    currency_file = open("%s.txt" % currency, 'w')
-    for num in xrange(int(begin), int(end)):
+    rand = random.randint(1, int(end))
+    for num in xrange(int(begin), int(end), rand):
+        print num, ":"
         os.system("./slownie %s %s" % (str(num), currency))
 
 test_currency(*sys.argv[1:])
